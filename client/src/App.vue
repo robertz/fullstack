74cdd896ee8317c034a8639e448d685ffc49cabe
Robert Zehnder <robert@kisdigital.com>
2018-03-22 10:41:54 -0400

    Initial Deploy

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <div>
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+      </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
