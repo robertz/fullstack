4a2437c8abf6653929869392763804fcf6100aef
Robert Zehnder <robert@kisdigital.com>
2018-03-22 14:48:45 -0400

    Node working

---
+++ server/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('You found the tater');
+});
+
+app.listen(PORT, () => {
+    console.log(`App started and listening on port ${PORT}`)
+});
